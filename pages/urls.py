18b389800ba759e6aff9683732cd83c6afeb25bb
kampfgockel <martintsmith347@gmail.com>
2022-06-26 19:06:32 -0600

    Added Country tracker page

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tracker', views.tracker, name='tracker'),
     path('about', views.about, name='about'),
     path('chart', views.chart, name='chart'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from orders.models import Order
 from shippers.models import Shipper
-from django.db.models import Count
+from suppliers.models import Supplier
+from customers.models import Customer
+from products.models import Product
+from orders.models import Order
+from employees.models import Employee
+from django.db.models import Count, Avg
 import json
-
+from .data import countries
 def index(request):
-    context = {}
+    context = {}    
     return render(request, 'pages/index.html', context)
+
+def tracker(request):
+    customers = Customer.objects.all()
+    employees = Employee.objects.all()
+    suppliers = Supplier.objects.all()
+    shippers = Shipper.objects.all()
+    context = {
+        'countries': countries,
+        'customers': customers,
+        'employees': employees,
+        'suppliers': suppliers,
+        'shippers': shippers,
+        }
+    return render(request, 'pages/tracker.html', context)
 
 
 def about(request):
